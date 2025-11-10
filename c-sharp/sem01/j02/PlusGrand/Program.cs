fe6a2f7386303ca38e5260a52b423a12c2e15254
Laurent Larsonneur <laurent.larsonneur@laposte.net>
2025-11-10 11:35:57 +0100

    Ajout du programme C# Plus-Grand

---
+++ c-sharp/sem01/j02/PlusGrand/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Entrez le nombre 1 : ");
+        int n1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Entrez le nombre 2 : ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        if (n1 == n2)
+            Console.WriteLine("Les nombres sont égaux");
+        else
+            if (n1 > n2)
+            Console.WriteLine($"{n1} est le plus grand nombre");
+        else
+            Console.WriteLine($"{n2} est le plus grand nombre");
+    }
+}
