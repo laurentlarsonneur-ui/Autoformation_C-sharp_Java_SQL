a2592ccc081319bc55696b20145e49cfa46ae904
Laurent Larsonneur <laurent.larsonneur@laposte.net>
2025-11-10 11:49:14 +0100

    Ajout du programme C# Fonctions

---
+++ c-sharp/sem01/j02/Fonctions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int Somme(int[] tab)
+    {
+        int total = 0;
+        foreach (int x in tab) total += x;
+        return total;
+    }
+    static double moyenne(int[] t)
+    {
+        return ((double)Somme(t) / t.Length);
+    }
+    static void Main()
+    {
+        int[] valeurs = { 1, 3, 6 };
+        Console.WriteLine("Somme = " + Somme(valeurs));
+        Console.WriteLine($"Moyenne = {moyenne(valeurs)}");
+    }
+}
