3f847a78eceaa428ce45070a081cdfe24272d10a
Laurent Larsonneur <laurent.larsonneur@laposte.net>
2025-11-10 11:40:27 +0100

    Ajout du programme C# Moyenne

---
+++ c-sharp/sem01/j02/Moyenne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] notes = { 12, 15, 9, 18, 14 };
+        int somme = 0;
+
+        for (int i = 0; i < notes.Length; i++)
+            somme += notes[i];
+
+        double moyenne = (double)somme / notes.Length;
+        Console.WriteLine($"Moyenne = {moyenne}");
+    }
+}
