94caf509e6a48aba5d5f0ffd57db824cfe41734a
Laurent Larsonneur <laurent.larsonneur@laposte.net>
2025-11-10 11:32:34 +0100

    Ajout du programme Pair-Impair

---
+++ c-sharp/sem01/j02/Pair-Impair/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Entrez un nombre : ");
+        int x = int.Parse(Console.ReadLine());
+
+        if (x % 2 == 0)
+            Console.WriteLine($"{x} est pair");
+        else
+            Console.WriteLine($"{x} est impair");
+    }
+}
