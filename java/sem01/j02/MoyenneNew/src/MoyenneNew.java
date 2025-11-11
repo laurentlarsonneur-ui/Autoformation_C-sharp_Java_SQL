cd10e7b731e7a2277624ed74557318036eb8dd04
Laurent Larsonneur <laurent.larsonneur@laposte.net>
2025-11-11 08:25:20 +0100

    Ajout du projet Java MoyenneNew

---
+++ java/sem01/j02/MoyenneNew/src/MoyenneNew.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MoyenneNew {
+    public static void main(String[] args) {
+        int[] notes ;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entrez le nombre de notes : ");
+        int n = sc.nextInt();
+        notes = new int[n];
+        for (int i = 0; i < notes.length; i++) {
+            System.out.print("Entrez la note " + (i+1) + " : ");
+            notes[i] = sc.nextInt();
+        }
+
+        int somme = 0;
+        for (int note : notes) {
+            somme += note;
+        }
+
+        double moy = (double)somme / notes.length;
+        System.out.println("Moyenne = " + moy);
+
+        sc.close();
+    }
+}
