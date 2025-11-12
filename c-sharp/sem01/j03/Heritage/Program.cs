c92bfefce30fac1ca5514a622f3d55c9a6424840
llarsonn <84820677+llarsonn@users.noreply.github.com>
2025-11-12 18:51:48 +0100

    Ajout du programme C# Heritage

---
+++ c-sharp/sem01/j03/Heritage/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Shape
+{
+    public virtual double Area() => 0;
+    public virtual void Display() => Console.WriteLine("Forme générique");
+}
+
+public class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double Area() => Width * Height;
+
+    public override void Display()
+    {
+        Console.WriteLine($"Rectangle {Width}x{Height} - Aire = {Area()}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape s = new Rectangle(4, 5);
+        s.Display();
+    }
+}
